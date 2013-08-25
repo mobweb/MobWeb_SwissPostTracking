014c9594e2b2bd37ca5cca8fab0028d8270b2916
Louis Bataillard <info@mobweb.ch>
2013-08-25 17:25:32 +0200

    initial commit

---
+++ app/code/local/MobWeb/SwissPostTracking/Block/Sales/Order/Shipment/View/Tracking.php
@@ -0,0 +1,15 @@
+<?php
+
+class MobWeb_SwissPostTracking_Block_Sales_Order_Shipment_View_Tracking extends Mage_Adminhtml_Block_Sales_Order_Shipment_View_Tracking
+{
+    public function getCarriers()
+    {
+        // Get the carriers from the original method. Remove the following
+        // line if you don't need Magento's default carriers listed here
+        $carriers = parent::getCarriers();
+
+        // Add the custom "Swiss Post" carrier to the list
+        $carriers['Swiss_Post'] = $this->__('Swiss Post');
+        return $carriers;
+    }
+}
